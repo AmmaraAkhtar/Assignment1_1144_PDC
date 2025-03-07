69c68d5807ca7fd103c47d7760b5d3cb5b032af7
AmmaraAkhtar <1144ammara@gmail.com>
2025-03-07 13:24:18 +0500

    Declare the of sizes and threads

---
+++ Task1_2_3.c
@@ -0,0 +1,20 @@
+// standard input/output library, Needed for printf() 
+#include<stdio.h>
+//standard library Needed fpr Srand() and rand()
+#include <stdlib.h>
+// this is time related function ,Needed for time(), clock()
+#include <omp.h>
+//include omp for parallelization
+#include<time.h>
+
+
+
+int main(){
+int sizes[]={512,1024,2000};//Test different array sizes
+int threads[]={1,4,8};// Test different thread sizes
+
+
+
+
+    return 0;
+}
