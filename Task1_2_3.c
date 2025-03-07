2f044bc1e65d616403acbbea04634fa0543c13cf
AmmaraAkhtar <1144ammara@gmail.com>
2025-03-07 13:40:21 +0500

    Declare the of sizes and threads

--- Task1_2_3.c
+++ Task1_2_3.c
@@ -6,14 +6,15 @@
 #include <omp.h>
 //include omp for parallelization
 #include<time.h>
-
+#define MAX_SIZE 2000//define maximum array 
 
 
 int main(){
 int sizes[]={512,1024,2000};//Test different array sizes
 int threads[]={1,4,8};// Test different thread sizes
 
-
+ static double A[MAX_SIZE][MAX_SIZE];    //static move A to static memory which hold much large data
+ //useful for large array that not fit on stack memory
 
 
     return 0;
